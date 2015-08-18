68b91c8593477f3515d4d5fc343fc17e2e2dae9d
phillip50 <phil50@ufl.edu>
2015-08-18 12:55:59 -0400

    general bug fixes

--- SyncMaster.java
+++ SyncMaster.java
@@ -7,66 +7,41 @@ public class SyncMaster
 {
     public static void main(String[] args)
     {
+        System.out.println("Hello");
+        
         File gatorFile;
         Database gatordb;
         Table gatorTable = null;
         File masterFile;
         Database masterdb;
         Table masterTable = null;
+        File startFile;
+        BufferedReader reader;
         
         try
         {
             gatorFile = new File("AnimalDatabase.accdb");
             gatorTable = DatabaseBuilder.open(gatorFile).getTable("Database");
             masterFile = new File("MasterAnimalDatabase.accdb");
-            masterTable = DatabaseBuilder.open(masterFile).getTable("Master");
+            masterTable = DatabaseBuilder.open(masterFile).getTable("Database");
+            startFile = new File("start.txt");
+            reader = new BufferedReader(new FileReader(startFile));
+            
+            int start = Integer.parseInt(reader.readLine());
+            reader.close();
+            System.out.println("" + start);
             
             Cursor gatorCursor = CursorBuilder.createCursor(gatorTable);
-            Cursor masterCursor = CursorBuilder.createCursor(masterTable);
             
             gatorCursor.beforeFirst();
-            masterCursor.beforeFirst();
             
-            while (gatorCursor.moveToNextRow())
-            {
-                if (!gatorCursor.isCurrentRowDeleted())
-                {
-                    Row row = gatorCursor.getCurrentRow();
-                    masterTable.addRow(0, row.get("Tag Number"), row.get("Egg Collection Date"), row.get("Egg Nest Location"), row.get("Egg Number"), row.get("Egg Length"), row.get("Egg Weight"), row.get("Hatch Year"), row.get("Gender"), row.get("Umbilical"), row.get("Date"), row.get("From"), row.get("To"), row.get("Belly Size"), row.get("Length"), row.get("Weight"), row.get("Special Formula"), row.get("Experiment Code"), row.get("Vaccinated"), row.get("Comments"), row.get("Harvested?"));
-                }
-            }
-            /*
-            boolean isDone = false;
-            
-            while (!isDone)
-            {
-                if (masterCursor.moveToNextRow() && gatorCursor.moveToNextRow())
-                {
-                    Row gatorRow = gatorCursor.getCurrentRow();
-                    Row masterRow = masterCursor.getCurrentRow();
-                    
-                    if (gatorRow.get("ID").toString().equals(masterRow.get("ID").toString()))
-                    {
-                        
-                    }
-                    else
-                    {
-                        isDone = true;
-                    }
-                }
-                else 
-                {
-                    isDone = true;
-                }
-            }
-            
-            gatorCursor.moveToPreviousRow();
+            gatorCursor.moveNextRows(start);
             
             while (gatorCursor.moveToNextRow())
             {
                 Row row = gatorCursor.getCurrentRow();
                 masterTable.addRow(0, row.get("Tag Number"), row.get("Egg Collection Date"), row.get("Egg Nest Location"), row.get("Egg Number"), row.get("Egg Length"), row.get("Egg Weight"), row.get("Hatch Year"), row.get("Gender"), row.get("Umbilical"), row.get("Date"), row.get("From"), row.get("To"), row.get("Belly Size"), row.get("Length"), row.get("Weight"), row.get("Special Formula"), row.get("Experiment Code"), row.get("Vaccinated"), row.get("Comments"), row.get("Harvested?"));
-            }*/
+            }   
         }
         catch (IOException e)
         {
